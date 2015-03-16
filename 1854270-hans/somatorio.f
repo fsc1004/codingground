624e82853e279cfb3edda794848fc799b26f15b1
Fortran UFSM <fsc1004@outlook.com>
2015-03-16 16:12:38 -0300

    Created by Coding Ground Client

---
+++ 1854270-hans/somatorio.f
@@ -0,0 +1,14 @@
+c23456789
+      program somatorio
+      implicit none
+      real soma
+      integer i
+      
+      do 10 i = 1,5
+        soma = soma + i
+        write(*,*)'i = ',i
+        write(*,*)'soma = ', soma
+10    continue
+      
+      stop 'Programa finalizado.'
+      end program somatorio
