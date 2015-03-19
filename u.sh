956dad7b38d661fa0533cc94206d734c3cad9bcc
fsc1004 <fsc1004@outlook.com>
2015-03-18 20:19:35 -0400

    atualizado

---
+++ u.sh
@@ -0,0 +1,12 @@
+#!/bin/bash                                                                                                                                                    
+                                                                                                                                                               
+git config --global user.email 'fsc1004@outlook.com'                                                                                                           
+git config --global user.name 'fsc1004'                                                                                                                        
+git pull                                                                                                                                                       
+git add -A && git commit -m 'atualizado'                                                                                                                       
+git push origin master                                                                                                                                         
+echo ''                                                                                                                                                        
+echo '----------------------------------------------'                                                                                                          
+echo '   Arquivos sincronizados com  repostorio                                                                                                                                                   
+echo '----------------------------------------------'  
+echo ''
