d7c0219f62f222cd68d2f5749153c4e6c4ce84b0
fsc1004 <fsc1004@outlook.com>
2015-03-19 09:13:08 -0400

    atualizado

---
+++ 1854270-hans/ex01.f
@@ -0,0 +1,10 @@
+c234567890
+	program ex01
+	integer n1, n2, s
+	
+	write(*,*)"digite dois numeros"
+	read(*,*)n1,n2
+	s = n1 + n2
+	write(*,*)"Soma = ",s
+	stop "Programa finalizado"
+	end program ex01
