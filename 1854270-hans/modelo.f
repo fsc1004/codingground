bba2e349803252beea0f769dabf7e469e756e28a
fsc1004 <fsc1004@outlook.com>
2015-03-18 21:09:51 -0400

    atualizado

--- 1854270-hans/modelo.f
+++ 1854270-hans/modelo.f
@@ -1,4 +1,9 @@
 c23456789
+!------------------------------
+! Arquivo: modelo.f
+! Desc: Este codigo fonte é apenas
+!       um modelo.
+!------------------------------
       program modelo
-      print*, "Modelo de programa em Fortran 77"
+      print*, "Modelo de programa em Fortran 77 "
       end program modelo
--- 1854270-hans/modelo.f95
+++ 1854270-hans/modelo.f95
@@ -1,4 +1,3 @@
 program modelo
    Print *, "Modelo de programa em Fortran 95!"
 end program modelo
-
