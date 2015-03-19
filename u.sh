76111351974d6cdc2a5599b66b808bfce7dc0a9b
fsc1004 <fsc1004@outlook.com>
2015-03-18 20:24:41 -0400

    atualizado

--- u.sh
+++ u.sh
@@ -1,12 +1,17 @@
-#!/bin/bash                                                                                                                                                    
-                                                                                                                                                               
-git config --global user.email 'fsc1004@outlook.com'                                                                                                           
-git config --global user.name 'fsc1004'                                                                                                                        
-git pull                                                                                                                                                       
-git add -A && git commit -m 'atualizado'                                                                                                                       
-git push origin master                                                                                                                                         
-echo ''                                                                                                                                                        
-echo '----------------------------------------------'                                                                                                          
-echo '   Arquivos sincronizados com  repostorio                                                                                                                                                   
-echo '----------------------------------------------'  
+#!/bin/bash
+clear
+echo ''
+echo '----------------------------------------------'
+echo '   SINCRONIZANDO ARQUIVOS COM O REPOSITORIO   '
+echo '----------------------------------------------'
+echo ''
+git config --global user.email 'fsc1004@outlook.com'
+git config --global user.name 'fsc1004'
+git pull
+git add -A && git commit -m 'atualizado'
+git push origin master
+echo ''
+echo '----------------------------------------------'
+echo '   Arquivos sincronizados com  repostorio     '
+echo '----------------------------------------------'
 echo ''
