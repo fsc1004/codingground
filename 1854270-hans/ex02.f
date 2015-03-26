f62d62391a3772b14b85b56ef3f85441e1e766de
fsc1004 <fsc1004@outlook.com>
2015-03-26 11:21:10 -0400

    atualizado

--- 1854270-hans/ex02.f
+++ 1854270-hans/ex02.f
@@ -2,12 +2,14 @@ c234567890
 	program ex01
 	real n1, n2, s
 	real media
+	logical aprovado
+	
 	write(*,*)"Digite duas notas"
 	read(*,*)n1,n2
 	media = (n1 + n2 ) / 2
 	write(*,*)"Media = ", media
-	
-	if (media .ge. 7.) then
+	aprovado = (media .ge. 7.)
+	if aprovado then
 	 	write(*,*)"O aluno foi aprovado"
 	else
 		write(*,*)"O aluno foi reprovado"
