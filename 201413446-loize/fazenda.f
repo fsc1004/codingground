f7d60df0c07d6715a3d3966631b282138bcb020a
fsc1004 <fsc1004@outlook.com>
2015-04-01 17:06:40 -0400

    atualizado

---
+++ 201413446-loize/fazenda.f
@@ -0,0 +1,18 @@
+	program fazenda
+	
+	write(*,*)"Eu vou contar minhas aves"
+	write(*,*)"galinhas",25+30/6
+	write(*,*)"galos",100-25*3/4
+	write(*,*)"Agora vou contar os ovos"
+	write(*,*)3+2+1-5+4/2-1/4+6
+	write(*,*)"É verdade que 3+2 < 5-7?",3+2.lt.5-7
+	write(*,*)"Quanto é 3+2?",3+2
+	write(*,*)"Quanto é 5-7?",5-7
+	write(*,*)"Ah, por isso que é falso"
+	write(*,*)"Que tal um pouco mais"
+	write(*,*)"Quem é maior 5>-2?",5.gt.-2
+	write(*,*)"Quem é maior ou igual 5>=2",5.ge.2
+	write(*,*)"Quem é menor ou igual 5<=-2?"5.le.2
+	
+	stop"Programa finalizado"
+	end
---
+++ 201512504-mario/fazenda.f
@@ -0,0 +1,18 @@
+	program fazenda
+	
+	write(*,*)"eu vou contar minhas aves"
+		write(*,*)"galinhas",25+30/6
+		write(*,*)"galos",100-25*3/4
+	write(*,*)"agora vou contar os ovos"
+		write(*,*)3+2+1-5+4/2-1/4+6
+		write(*,*)"é verdade que 3+2<5-7?",3+2.lt.5-7
+		write(*,*)"quanto é 3+2?",3+2
+		write(*,*)"quanto é 5-7?",5-7
+		write(*,*)"ah, por isso é que é falso"
+		write(*,*)"que tal um pouco mais"
+		write(*,*)"quem é maior 5>-2?",5.gt.-2
+		write(*,*)"quem é maior ou igual 5>=2?",5.ge.2
+		write(*,*)"quem é menor ou igual 5<=-2?",5.le.2
+	
+	stop"programa finalizado"
+	end
